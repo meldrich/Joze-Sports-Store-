43364aa9470bd22eb50343f17d853de5a36b09a3
meldrich <meldrich.jose@gmail.com>
2025-11-29 14:52:13 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer style="text-align:center; margin-top:30px;">
+    <p>&copy; <?php echo date("Y"); ?> Joze Sports Store | Created by: JOSE, Adrian Meldrich C.</p>
+</footer>
+</body>
+</html>
