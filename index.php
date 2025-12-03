bd202e5b60d602f83c072d2a9e30dbbac50c1d21
meldrich <meldrich.jose@gmail.com>
2025-12-03 21:28:25 +0800

    Update and rename JoseStore.php to index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require 'header.php';
+?>
+
+<div style="text-align:center; margin-top:50px;">
+    <h2>Welcome! If I had a store this is how I would design it and these are the products that I will sell for my business.</h2>
+    <p>
+        Go to <a href="products.php" style="color:#00eaff;">Products</a> 
+        or <a href="stock_monitor.php" style="color:#00eaff;">Stock Levels</a>.
+    </p>
+</div>
+
+<?php
+require 'footer.php';
+?>
+
