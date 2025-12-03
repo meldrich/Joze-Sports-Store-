bd845ff459f515d006b2fc0c3d71408cc634e5c6
meldrich <meldrich.jose@gmail.com>
2025-12-03 21:31:18 +0800

    Update products.php

--- products.php
+++ products.php
@@ -1,10 +1,8 @@
 <?php
-// Joze Track Sport Store Database
+require 'header.php';
 
-$storeName = "Joze Sports Store";
-$discount = 0.10;
+$discount = 0.1;
 
-// Joze Watches Data
 $watches = [
     ["Joze Forerunner 55", 10995, 12],
     ["Joze Venu Sq 2", 16995, 8],
@@ -12,7 +10,6 @@ $watches = [
     ["Joze Fenix 7S", 41995, 4]
 ];
 
-// Joze Shoes Data
 $shoes = [
     ["Joze Runner X1", 3495, 20],
     ["Joze Sprint Pro", 4995, 15],
@@ -20,3 +17,64 @@ $shoes = [
     ["Joze LightStep 2.0", 2595, 25]
 ];
 ?>
+
+<div class="section-label">JOZE WATCHES</div>
+<table>
+    <tr>
+        <th>Model</th>
+        <th>Original Price</th>
+        <th>Discounted Price</th>
+        <th>Stock</th>
+    </tr>
+<?php
+for ($i = 0; $i < count($watches); $i++) {
+    $discounted = $watches[$i][1] * (1 - $discount);
+
+    if ($watches[$i][2] >= 10) {
+        $stockMsg = "Good availability";
+    } elseif ($watches[$i][2] > 0) {
+        $stockMsg = "Low stock";
+    } else {
+        $stockMsg = "Out of stock";
+    }
+
+    echo "<tr>
+            <td>".$watches[$i][0]."</td>
+            <td>Php ".$watches[$i][1]."</td>
+            <td>Php ".$discounted."</td>
+            <td>".$watches[$i][2]." pcs - ".$stockMsg."</td>
+          </tr>";
+}
+?>
+</table>
+
+<div class="section-label">JOZE SHOES</div>
+<table>
+    <tr>
+        <th>Model</th>
+        <th>Original Price</th>
+        <th>Discounted Price</th>
+        <th>Stock</th>
+    </tr>
+<?php
+$j = 0;
+while ($j < count($shoes)) {
+    $discounted = $shoes[$j][1] * (1 - $discount);
+    $stockMsg = ($shoes[$j][2] >= 10) ? "Good availability" : "Low stock";
+
+    echo "<tr>
+            <td>".$shoes[$j][0]."</td>
+            <td>Php ".$shoes[$j][1]."</td>
+            <td>Php ".$discounted."</td>
+            <td>".$shoes[$j][2]." pcs - ".$stockMsg."</td>
+          </tr>";
+
+    $j++;
+}
+?>
+</table>
+
+<?php
+require 'footer.php';
+?>
+
